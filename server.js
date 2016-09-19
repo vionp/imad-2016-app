41e46fe842f9101941b2b0aabd13e38b9bcdf9dd
vionp <vineeth.pillai@gmail.com>
2016-09-19 10:41:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function(req,res){
+  res.send('Article 1 requested and will be served from here');
+});
+
+app.get('/article-two', function(req,res){
+  res.send('Article 2 requested and will be served from here');
+});
+
+app.get('/article-three', function(req,res){
+  res.send('Article 3 requested and will be served from here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
