dad9cfeb5e5fda68b4844186ba4e4bd00628e512
vionp <vineeth.pillai@gmail.com>
2016-09-22 23:19:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,26 @@
 //Counter code
 var button = document.getElementById('counter');
-var counter = 0;
+
 
 button.onclick = function() {
     
-    
-    //make a request to the counter end point
+    //create a request object
+    var request = new XMLHttpRequest();
     
     //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+    if (request.readyState === XMLHttpRequest.DONE) {
+        //Take some action
+    if (request.status === 200) {
+        var counter = request.responseText;
+        var span = document.getElementById('count')
+        span.innerHTML = counter.toString();
+        }
+    }
+        //not done yet
+    };
     
-    //render the variable in the correct span
-    counter = counter +1;
-    var span = document.getElementById('count')
-    span.innerHTML = counter.toString();
-};
+    //Make the request
+    request.Open('Get','http://vionp.imad.hasura-app.io/counter',true)
+    request.send(null);
+}
