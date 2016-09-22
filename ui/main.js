05b54b1f341d1ba9f084a0edc9e6f655ac4d20f9
vionp <vineeth.pillai@gmail.com>
2016-09-22 11:46:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 //change text to add a new value entry
 var element=document.getElementById('main-text');
 element.innerHTML = 'New value is added';
+
+//writing the code that will move the image when you click on it
+var img= document.getElementById('img');
+img.onclick = function() {
+    img.style.marginLeft = '100px';
+};
