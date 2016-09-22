cfe45ae2f6e3e45bdc55e47f7087659cbf172ae9
vionp <vineeth.pillai@gmail.com>
2016-09-22 11:39:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -37,6 +37,10 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
 app.get('/ui/Ppps.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Ppps.jpg'));
 });
