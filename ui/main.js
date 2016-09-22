9ce534ba60af8f7cb4b7feb3d1ae42d4c0d4eb98
vionp <vineeth.pillai@gmail.com>
2016-09-22 12:02:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,6 @@ function moveRight() {
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function() {
-    
     var interval = setInterval(moveRight,100)
     img.style.marginLeft = '200px';
 };
