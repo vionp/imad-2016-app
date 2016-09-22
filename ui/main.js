3a467099f0e5b1ffbc236f32158d7b655705aebb
vionp <vineeth.pillai@gmail.com>
2016-09-22 11:22:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 //change text to add a new value entry
-var element=document.getElementbyID ('main-text');
+var element=document.getElementById('main-text');
 element.innerhtml='New value is added';
