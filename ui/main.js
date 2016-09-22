aef1f25149a9810a3a3afa32cceb84d8bc3ef6ea
vionp <vineeth.pillai@gmail.com>
2016-09-22 12:10:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ element.innerHTML = 'New value is added';
 var img= document.getElementById('imagemove');
 var marginLeft = 0;
 function moveRight() {
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function() {
