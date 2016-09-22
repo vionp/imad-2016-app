9f19fd0a7931ea02cd4af0f4155c70cd26225797
vionp <vineeth.pillai@gmail.com>
2016-09-22 11:59:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,13 @@ element.innerHTML = 'New value is added';
 
 //writing the code that will move the image when you click on it
 var img= document.getElementById('imagemove');
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function() {
+    
     var interval = setInterval(moveRight,100)
     img.style.marginLeft = '200px';
 };
